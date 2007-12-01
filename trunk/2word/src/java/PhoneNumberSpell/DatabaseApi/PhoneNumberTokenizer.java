84fadd4616a1f1c972872b4065b18d7db2f79069
nadav.azaria <nadav.azaria@ce8596f2-d93f-0410-8ebe-f1572db582e0>
2007-12-01 20:59:55 +0000

    add class which enable to parse a phone number into a wordable and a non wordable tokens. Add tests to class.

--- trunk/2word/src/java/PhoneNumberSpell/DatabaseApi/PhoneNumberTokenizer.java
+++ trunk/2word/src/java/PhoneNumberSpell/DatabaseApi/PhoneNumberTokenizer.java
@@ -15,8 +15,58 @@ package PhoneNumberSpell.DatabaseApi;
  */
 public class PhoneNumberTokenizer {
     
+    String number;
+    int reference = 0;
     /** Creates a new instance of PhoneNumberTokenizer */
-    public PhoneNumberTokenizer() {
+    public PhoneNumberTokenizer(String number) {
+        this.number = number;
     }
+    
+    public String next(){
+        String result;
+        if (isStartOftoken(number,reference)){
+            result = getTokenStartAt(number,reference);            
+        }
+        else{
+            result = getNonTokenStartAt(number,reference);
+        }
+        reference = reference + result.length();
+        return result;      
+    }
+    
+    boolean isConvertableChar(String c){
+        return (c.compareToIgnoreCase("0") != 0 && c.compareToIgnoreCase("1") != 0 );
+    }
+    
+    boolean isStartOftoken(String t,int i){
+        if (t.length() < i+2 )
+            return false;
+        else{
+            String s1=t.substring(i,i+1);
+            String s2 = t.substring(i+1,i+2);
+            
+            boolean b1 = isConvertableChar(s1);
+            boolean b2 = isConvertableChar(s2);
+            //System.out.println("isStartOftoken " + s1 + " " + s2 + " " + b1 + " " + b2);
+            return  b1&&b2;
+        }
+    }
+    String getTokenStartAt(String t, int i){
+        for (int j = i + 2 ; j < t.length() ; j++ ){
+            if (!isConvertableChar(t.substring(j,j+1)))
+                return t.substring(i,j);
+        }
+        return t.substring(i);
+    }
+
+    String getNonTokenStartAt(String number, int reference) {
+        int endsAt = reference+1;
+        while (endsAt < number.length() && !isStartOftoken(number,endsAt)   ){
+            endsAt++;
+        }
+        return number.substring(reference,endsAt);
+    }
+    
+    
     
 }
---
+++ trunk/2word/test/PhoneNumberSpell/DatabaseApi/PhoneNumberTokenizerTest.java
@@ -0,0 +1,120 @@
+/*
+ * PhoneNumberTokenizerTest.java
+ * JUnit based test
+ *
+ * Created on November 29, 2007, 9:39 PM
+ */
+
+package PhoneNumberSpell.DatabaseApi;
+
+import junit.framework.*;
+
+/**
+ *
+ * @author Nadav
+ */
+public class PhoneNumberTokenizerTest extends TestCase {
+    
+    public PhoneNumberTokenizerTest(String testName) {
+        super(testName);
+    }
+
+    protected void setUp() throws Exception {
+    }
+
+    protected void tearDown() throws Exception {
+    }
+
+    /**
+     * Test of next method, of class PhoneNumberSpell.DatabaseApi.PhoneNumberTokenizer.
+     */
+    public void testNext1() {
+        System.out.println("next1");
+        
+        PhoneNumberTokenizer instance = new PhoneNumberTokenizer("00115505033034514320");
+        
+        String expResult = "0011";
+        String result = instance.next();
+        System.out.println(expResult + " " + result);
+        assertEquals(expResult, result);  
+        
+        expResult = "55";
+        result = instance.next();
+        System.out.println(expResult + " " + result);
+        assertEquals(expResult, result);       
+        
+        expResult = "050";
+        result = instance.next();
+        System.out.println(expResult + " " + result);
+        assertEquals(expResult, result);       
+        
+        expResult = "33";
+        result = instance.next();
+        System.out.println(expResult + " " + result);
+        assertEquals(expResult, result);       
+        
+        expResult = "0";
+        result = instance.next();
+        System.out.println(expResult + " " + result);
+        assertEquals(expResult, result);  
+        
+        expResult = "345";
+        result = instance.next();
+        System.out.println(expResult + " " + result);
+        assertEquals(expResult, result);  
+        
+        expResult = "1";
+        result = instance.next();
+        System.out.println(expResult + " " + result);
+        assertEquals(expResult, result);  
+        
+        expResult = "432";
+        result = instance.next();
+       System.out.println(expResult + " " + result);
+        assertEquals(expResult, result);  
+        
+        expResult = "0";
+        result = instance.next();
+        System.out.println(expResult + " " + result);
+        assertEquals(expResult, result);  
+        
+    }
+    
+    public void testisConvertableChar() {
+        PhoneNumberTokenizer instance = new PhoneNumberTokenizer("001155");
+         assertEquals(false,instance.isConvertableChar("0"));
+         assertEquals(false,instance.isConvertableChar("1"));
+         assertEquals(true,instance.isConvertableChar("2"));
+         assertEquals(true,instance.isConvertableChar("3"));
+         assertEquals(true,instance.isConvertableChar("4"));
+         assertEquals(true,instance.isConvertableChar("5"));
+         assertEquals(true,instance.isConvertableChar("6"));
+         assertEquals(true,instance.isConvertableChar("7"));
+         assertEquals(true,instance.isConvertableChar("8"));
+         assertEquals(true,instance.isConvertableChar("9"));       
+         
+    }
+    
+    
+    public void testisStartOftoken() {
+               
+        PhoneNumberTokenizer instance = new PhoneNumberTokenizer("010156");
+        
+        boolean expResult = true;
+       System.out.println("start testisStartOftoken");
+        boolean result = instance.isStartOftoken("010156",4);
+        System.out.println("end testisStartOftoken");
+        assertEquals(expResult, result);         
+        expResult = false;
+        result = instance.isStartOftoken("010156",0);
+        assertEquals(expResult, result); 
+        result = instance.isStartOftoken("010156",1);
+        assertEquals(expResult, result); 
+        result = instance.isStartOftoken("010156",2);
+        assertEquals(expResult, result); 
+        result = instance.isStartOftoken("010156",3);
+        assertEquals(expResult, result);        
+        
+    }
+    
+}
